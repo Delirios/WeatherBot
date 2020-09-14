ca534b1525c53a2084ebc0fc39e52f7a1c13ddc8
Delirios <lushchevskiyd@gmail.com>
2020-09-14 21:47:24 +0300

    Add project files.

---
+++ WeatherBot/Bots/WeatherBot.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+
+namespace WeatherBot.Bots
+{
+    public class WeatherBot : ActivityHandler
+    {
+    }
+}
